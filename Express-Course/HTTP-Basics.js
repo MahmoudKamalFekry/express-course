afd075b9cc402d4c2876e4abed53ec459af759c3
Mahmoud Kamal <mahmoudkamal30000@gmail.com>
2026-02-26 03:47:06 +0100

    HTTP-Basics

---
+++ Express-Course/HTTP-Basics.js
@@ -0,0 +1,12 @@
+const http = require ("http")
+
+const server = http.createServer((req , res)=>{
+console.log("the user hit the server");
+
+res.end("Welcome To The Home page !")
+})
+
+
+server.listen(5000 ,() =>{
+  console.log("server listen on port 5000");
+})
